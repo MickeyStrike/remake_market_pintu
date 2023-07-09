20906106e85840a14297910d292464131750a0e9
irfanmaulana28 <muhamad.irfan@misteraladin.com>
2023-07-09 23:30:43 +0700

    fixed bug on subheader input

--- src/components/SubHeader.tsx
+++ src/components/SubHeader.tsx
@@ -14,11 +14,13 @@ const SubHeader:FC<ISubHeader> = ({ getData }) => {
     <div className='flex flex-row justify-between max-md:flex-col max-md:gap-4'>
       <h1 className='font-bold text-3xl max-md:text-xl max-lg:text-lg max-xl:text-2xl'>Harga Crypto dalam Rupiah Hari ini</h1>
       <div>
-        <div className='flex flex-row gap-2'>
-          <div className='flex flex-row bg-gray-100 py-3 px-4 rounded-lg'>
-            <Search />
+        <div className='md:flex md:flex-row md:gap-2'>
+          <div className='flex flex-row bg-gray-100 py-3 px-4 rounded-lg max-md:gap-2'>
+            <div>
+              <Search />
+            </div>
             <input
-              className='ml-4 bg-transparent focus:outline-none w-96'
+              className='md:ml-4 bg-transparent focus:outline-none w-96 max-md:w-full'
               type='text'
               placeholder='Cari aset di Pintu...'
               onChange={(e) => {
